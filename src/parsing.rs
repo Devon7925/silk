b8dd7aa9974b75a0b1c7599aef3c4f1159429e6e
Devon7925 <devonrutledge03@gmail.com>
2025-11-09 23:21:54 -0700

    Improve binding parsing

--- src/parsing.rs
+++ src/parsing.rs
@@ -16,6 +16,8 @@ pub enum Expression {
         left: Box<Expression>,
         right: Box<Expression>,
     },
+    Binding(Box<Binding>),
+    Block(Vec<Expression>),
 }
 
 pub struct Binding {
@@ -34,6 +36,10 @@ pub fn parse_eq(file: &str) -> Option<&str> {
 
 pub fn parse_type_decl(file: &str) -> Option<&str> {
     file.strip_prefix(":")
+}
+
+fn parse_semicolon(file: &str) -> Result<&str, String> {
+    file.strip_prefix(";").ok_or("Expected ;".to_string())
 }
 
 pub fn parse_whitespace(file: &str) -> Option<&str> {
@@ -96,7 +102,7 @@ pub fn parse_type_hint(file: &str) -> Option<Result<(Expression, &str), String>>
 pub fn parse_grouping_expression(file: &str) -> Option<(Expression, &str)> {
     let file = file.strip_prefix("(")?;
     let file = parse_optional_whitespace(file);
-    let (expr, file) = parse_operation_expression(file).ok()?;
+    let (expr, file) = parse_block(file).ok()?;
     let file = parse_optional_whitespace(file);
     let file = file.strip_prefix(")")?;
     Some((expr, file))
@@ -135,7 +141,10 @@ pub fn parse_isolated_expression(file: &str) -> Result<(Expression, &str), Strin
     if let Some((literal, remaining)) = parse_literal(file) {
         return Ok((Expression::Literal(literal), remaining));
     }
-    Err("Expected expression".to_string())
+    Err(format!(
+        "Expected expression at: {}",
+        file.chars().take(10).collect::<String>()
+    ))
 }
 
 pub fn parse_operation_expression(file: &str) -> Result<(Expression, &str), String> {
@@ -211,105 +220,104 @@ pub fn parse_operation_expression(file: &str) -> Result<(Expression, &str), Stri
     Ok((final_expression, remaining))
 }
 
-pub fn parse_binding(file: &str) -> Result<(Binding, &str), String> {
-    let file = parse_let(file).ok_or("Expected let keyword".to_string())?;
-    let file = parse_whitespace(file).ok_or("Expected whitespace after let".to_string())?;
-    let (pattern, file) = parse_binding_pattern(file)?;
-    let (type_hint, file) = parse_type_hint(file)
-        .map(|h| h.map(|(expr, file)| (Some(expr), file)))
-        .unwrap_or(Ok((None, file)))?;
-    let file = parse_optional_whitespace(file);
-    let file = parse_eq(file).ok_or("Expected = after binding pattern".to_string())?;
-    let file = parse_optional_whitespace(file);
-    let (expr, file) = parse_isolated_expression(file)?;
+pub fn parse_binding(file: &str) -> Option<Result<(Binding, &str), String>> {
+    let file = parse_let(file)?;
+    fn parse_binding(file: &str) -> Result<(Binding, &str), String> {
+        let file = parse_whitespace(file).ok_or("Expected whitespace after let".to_string())?;
+        let (pattern, file) = parse_binding_pattern(file)?;
+        let (type_hint, file) = parse_type_hint(file)
+            .map(|h| h.map(|(expr, file)| (Some(expr), file)))
+            .unwrap_or(Ok((None, file)))?;
+        let file = parse_optional_whitespace(file);
+        let file = parse_eq(file).ok_or("Expected = after binding pattern".to_string())?;
+        let file = parse_optional_whitespace(file);
+        let (expr, file) = parse_isolated_expression(file)?;
 
-    Ok((
-        Binding {
-            pattern,
-            type_hint,
-            expr,
-        },
-        file,
-    ))
+        Ok((
+            Binding {
+                pattern,
+                type_hint,
+                expr,
+            },
+            file,
+        ))
+    }
+    Some(parse_binding(file))
 }
 
-pub fn parse(file: &str) -> Result<Vec<Binding>, String> {
-    let mut bindings = Vec::new();
+pub fn parse_individual_expression(file: &str) -> Result<(Expression, &str), String> {
+    if let Some(binding_parse) = parse_binding(file) {
+        return binding_parse.map(|(binding, remaining)| (Expression::Binding(Box::new(binding)), remaining));
+    }
+    parse_operation_expression(file)
+}
+
+pub fn parse_block(file: &str) -> Result<(Expression, &str), String> {
+    let mut expressions = Vec::new();
     let mut remaining = file;
     remaining = parse_optional_whitespace(remaining);
 
+    let (expression, rest) = parse_individual_expression(remaining)?;
+    expressions.push(expression);
+    remaining = rest;
+    remaining = parse_optional_whitespace(remaining);
+
     while !remaining.trim().is_empty() {
-        let (binding, rest) = parse_binding(remaining)?;
-        bindings.push(binding);
+        let rest = parse_semicolon(remaining)?;
+        let rest = parse_optional_whitespace(rest);
+        let (expression, rest) = parse_individual_expression(rest)?;
+        expressions.push(expression);
         remaining = rest;
         remaining = parse_optional_whitespace(remaining);
     }
 
-    Ok(bindings)
+    if expressions.len() == 1 {
+        return Ok((expressions.into_iter().next().unwrap(), remaining));
+    }
+
+    Ok((Expression::Block(expressions), remaining))
 }
-
-
 
 #[test]
 fn parse_basic_let() {
-    let parsed = parse(
+    let parsed = parse_block(
         "
-let x = 42
+let x = 42;
 let y: i32 = x
     ",
     )
     .unwrap();
-    assert_eq!(parsed.len(), 2);
-    assert_eq!(
-        matches!(parsed[0].pattern, BindingPattern::Identifier(ref id) if id.0 == "x"),
-        true
-    );
-    assert_eq!(
-        matches!(parsed[0].expr, Expression::Literal(ExpressionLiteral::Number(lit)) if lit == 42),
-        true
-    );
-    assert_eq!(
-        matches!(parsed[1].pattern, BindingPattern::Identifier(ref id) if id.0 == "y"),
-        true
-    );
-    assert_eq!(
-        matches!(parsed[1].type_hint, Some(Expression::Identifier(ref hint)) if hint.0 == "i32"),
-        true
-    );
-    assert_eq!(
-        matches!(parsed[1].expr, Expression::Identifier(ref lit) if lit.0 == "x"),
-        true
-    );
-}
 
-#[test]
-fn parse_basic_let_grouped() {
-    let parsed = parse(
-        "
-let x = (42)
-let y: i32 = ((x))
-    ",
-    )
-    .unwrap();
+    let (Expression::Block(parsed), "") = parsed else {
+        panic!()
+    };
     assert_eq!(parsed.len(), 2);
+
+    let Expression::Binding(binding1) = &parsed[0] else {
+        panic!()
+    };
     assert_eq!(
-        matches!(parsed[0].pattern, BindingPattern::Identifier(ref id) if id.0 == "x"),
+        matches!(binding1.pattern, BindingPattern::Identifier(ref id) if id.0 == "x"),
         true
     );
     assert_eq!(
-        matches!(parsed[0].expr, Expression::Literal(ExpressionLiteral::Number(lit)) if lit == 42),
+        matches!(binding1.expr, Expression::Literal(ExpressionLiteral::Number(lit)) if lit == 42),
+        true
+    );
+
+    let Expression::Binding(binding2) = &parsed[1] else {
+        panic!()
+    };
+    assert_eq!(
+        matches!(binding2.pattern, BindingPattern::Identifier(ref id) if id.0 == "y"),
         true
     );
     assert_eq!(
-        matches!(parsed[1].pattern, BindingPattern::Identifier(ref id) if id.0 == "y"),
+        matches!(binding2.type_hint, Some(Expression::Identifier(ref hint)) if hint.0 == "i32"),
         true
     );
     assert_eq!(
-        matches!(parsed[1].type_hint, Some(Expression::Identifier(ref hint)) if hint.0 == "i32"),
-        true
-    );
-    assert_eq!(
-        matches!(parsed[1].expr, Expression::Identifier(ref lit) if lit.0 == "x"),
+        matches!(binding2.expr, Expression::Identifier(ref lit) if lit.0 == "x"),
         true
     );
 }
